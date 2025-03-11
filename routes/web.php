13dbf9b13ed9594de2ecaa029a5801c7ce7273e1
tomoya <tyamada4234@gmail.com>
2025-03-11 18:56:02 +0000

    課題13

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,13 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+    Route::get('news/delete', 'delete')->name('news.delete');
 });
 
 //上記Routing設定の別の書き方
@@ -36,11 +41,25 @@ Route::controller(NewsController::class)->prefix('admin')->group(function() {
 // })
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
 
     //admin/profile/create にアクセスしたら ProfileController の add Actionに割り当てる
-    Route::get('profile/create', 'add');
-    
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile', 'index')->name('profile.index');
     //admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てる
-    Route::get('profilee/edit', 'edit');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
+    Route::get('profile/delete', 'delete')->name('profile.delete');
 });
+
+//Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\NewsController as PublicNewsController;
+Route::get('/', [PublicNewsController::class, 'index'])->name('news.index');
