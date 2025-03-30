717aa60e3c489e2b3e682883c177a26d53d2144e
tomoya <tyamada4234@gmail.com>
2025-03-30 14:50:08 +0000

    mynews開発環境完了

---
+++ lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'login' => 'Login',
+    'logout' => 'Logout ',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Rememberme',
+];
---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<htmp lang="{{ app()->getlocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title')</title>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/front.css') }}" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav">
+                            {{-- 以下を追記 --}}
+                            <!-- Authentication Links -->
+                            {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                            @guest
+                                <li><a class="nav-link" href="{{ route('login') }}">ゲスト{{ __('messages.login') }}</a></li>
+                            {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                            {{ __('messages.logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+
+                        </ul>
+                    </div>
+                </div>
+
+            </nav>
+
+            <main class="py-4">
+                <p>abc</p>
+                @yield('content')
+                <p>def</p>
+            </main>
+
+        </div>
+    </body>
+</htmp>
